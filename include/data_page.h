f1e5c268bc821b0d974ada9c1cc203874b7a28ff
UUUtoma <842567642@qq.com>
2020-06-21 04:38:46 +0800

    Update pm_ehash: 1. update data_page.h 2. implement some functions in pm_ehash related to data_page (in data_page.cpp)

--- include/data_page.h
+++ include/data_page.h
@@ -1,6 +1,11 @@
 #ifndef DATA_PAGE
 #define DATA_PAGE
 
+#include<bitset>
+#include"pm_ehash.h"
+
+using std::bitset;
+
 #define DATA_PAGE_SLOT_NUM 16
 // use pm_address to locate the data in the page
 
@@ -9,6 +14,8 @@
 typedef struct data_page {
     // fixed-size record design
     // uncompressed page format
+    pm_bucket slots[DATA_PAGE_SLOT_NUM];
+	bitset<DATA_PAGE_SLOT_NUM> bitmap;
 } data_page;
 
 #endif
