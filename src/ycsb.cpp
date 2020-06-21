56d7b092fc5a12821b557aeab5bc60c8b0a8d7ea
CatherineDianaZhang <shuiyinxuanxue@163.com>
2020-06-21 16:22:46 +0800

    add some

--- src/ycsb.cpp
+++ src/ycsb.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <ctime>
+#include <string>
+#include "pm_ehash.h"
 using namespace std;
 
-void load(){
+void load(PmEHash* ehash){
+    ifstream in("");
+    if(!in){
+        cout << "error opening load file" << endl;
+        return;
+    }
 
+    string line;
+    string opera, temp;
+    uint64_t key, value;
+    while(getline(in, line)){
+        istringstream ss(line);
+        ss >> opera;
+        ss >> temp;
+        istringstream is(temp.substr(0,8));
+        is >> key;
+        
+    }
 }
 
-void run(){
+void run(PmEHash* ehash){
+    ifstream in("");
 
 }
 
 int mian(){
     double diff;
     clock_t start, end;
+    PmEHash* ehash = new PmEHash;
     start = clock();
-    load();
-    run();
+    load(ehash);
+    run(ehash);
     end = clock();
     diff = (double)(end - start);
     printf("Load time is : %f\n", diff / CLOCKS_PER_SEC);
